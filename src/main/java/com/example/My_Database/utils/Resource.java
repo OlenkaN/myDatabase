e3f6d153d2c3048531099876f07be7199c93b832
Olenka <namakaolenka22@gmail.com>
2022-11-07 23:46:09 +0200

    add Hateoas

---
+++ src/main/java/com/example/My_Database/utils/Resource.java
@@ -0,0 +1,12 @@
+package com.example.My_Database.utils;
+
+public class Resource {
+    public String link;
+    public String method;
+
+    public Resource(String link, String method){
+        this.link = link;
+        this.method = method;
+    }
+}
+
